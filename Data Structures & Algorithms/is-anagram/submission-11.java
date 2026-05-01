9b6e11ea58f602f912ccad0be5295acb11cc1089
akshayst369 <akshayst369@gmail.com>
2026-05-01 23:46:16 +0530

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+    if(s.length() != t.length())
+    {
+        return false;
+    }
+    int []count = new int[26];
+    for(char d : s.toCharArray())
+    {
+        count[ d -'a']++;
+    }
+    for(char c : t.toCharArray())
+    {
+        count[ c - 'a']--;
+    }
+    for(int i = 0;i<count.length;i++)
+    {
+        if(count[i]!=0)
+        return false;
+    }
+    return true;
+    }
+}
